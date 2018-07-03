d222b9f075441a62da90ef071adc3b72cb44773a
sxhx <sxhx@hwahae.co.kr>
2018-07-03 17:48:16 +0900

    Implement login, logout

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -5,16 +5,30 @@
     </a>
 
     <div class="right menu">
-      <a href="#" class="ui item" @click="login">Login</a>
+      <div v-if="isLoggedIn" class="horizontal">
+        <a class="item">Galleries</a>
+        <a class="item">Upload</a>
+        <a class="item" @click="logout">Logout</a>
+      </div>
+
+      <a v-else href="#" class="ui item" @click="login">Login</a>
     </div>
   </div>
 </template>
 
 <script>
-import { mapActions } from 'vuex';
+import { mapActions, mapGetters } from 'vuex';
 
 export default {
   name: 'AppHeader',
-  methods: mapActions(['login'])
+  computed: mapGetters(['isLoggedIn']),
+  methods: mapActions(['login', 'logout'])
 }
 </script>
+
+<style scoped>
+.horizontal {
+  display: flex;
+  flex-direction: row;
+}
+</style>
